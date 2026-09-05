2e261aa2c8afd2f5337a8deb6e51515955f0c5b8
gdpclothings-collab <gdpclothings@gmail.com>
2026-09-05 12:25:11 -0600

    Type shared Button component for checkJs

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -34,6 +34,8 @@ const buttonVariants = cva(
   }
 )
 
+/** @typedef {import("class-variance-authority").VariantProps<typeof buttonVariants> & React.ButtonHTMLAttributes<HTMLButtonElement> & { asChild?: boolean }} ButtonProps */
+/** @type {React.ForwardRefExoticComponent<ButtonProps & React.RefAttributes<HTMLButtonElement>>} */
 const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
   return (
