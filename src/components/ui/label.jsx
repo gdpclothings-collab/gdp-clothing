40d4173355e1222f373be0ed7ae03ba385bbad37
gdpclothings-collab <gdpclothings@gmail.com>
2026-09-05 12:25:08 -0600

    Type shared Label component for checkJs

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -8,6 +8,8 @@ const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
+/** @typedef {React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>} LabelProps */
+/** @type {React.ForwardRefExoticComponent<LabelProps & React.RefAttributes<React.ElementRef<typeof LabelPrimitive.Root>>>} */
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
 ))
