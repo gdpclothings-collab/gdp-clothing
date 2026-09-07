fc595651502bc019d19b314981821f21d5038f3c
gdpclothings-collab <gdpclothings@gmail.com>
2026-09-07 01:14:32 -0600

    Wire cookie preference center

--- src/components/storefront/Layout.jsx
+++ src/components/storefront/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import StoreNav from "./StoreNav";
 import StoreFooter from "./StoreFooter";
 import AIAssistant from "./AIAssistant";
+import CookiePreferences from "./CookiePreferences";
 
 export default function Layout() {
   return (
@@ -13,6 +14,7 @@ export default function Layout() {
       </main>
       <StoreFooter />
       <AIAssistant />
+      <CookiePreferences />
     </div>
   );
 }
