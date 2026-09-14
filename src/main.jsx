24648ac741cdc223a9b7c77926f143117b81b86e
gdpclothings-collab <gdpclothings@gmail.com>
2026-09-14 08:24:59 -0600

    feat: wire mobile-only Custom Studio refinement

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import '@/index.css'
 import '@/components/storefront/customStudioLayoutRefinement.css'
 import '@/components/storefront/customStudioLayoutPriority.css'
 import '@/components/storefront/customStudioTouchRefinement.css'
+import '@/components/storefront/customStudioMobileRefinement.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
