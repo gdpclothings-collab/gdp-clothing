be727606f1bd109641162c0fc3cf0b31019ec560
gdpclothings-collab <gdpclothings@gmail.com>
2026-09-24 22:00:53 -0600

    Hotfix storefront boot regression from admin cleanup mount

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
-import OrderCleanupControl from '@/components/admin/OrderCleanupControl.jsx'
 import '@/index.css'
 import '@/components/storefront/maintenanceMobileRepair.css'
 import '@/components/storefront/customStudioLayoutRefinement.css'
@@ -11,8 +10,5 @@ import '@/components/storefront/customStudioMobileRefinement.css'
 import '@/components/storefront/customStudioReviewedFixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-    <OrderCleanupControl />
-  </>
+  <App />
 )
